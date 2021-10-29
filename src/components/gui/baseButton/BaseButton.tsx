c14ed3537fdcc40efb118f88e754083db7a4f4ea
Lucas <lukasmolin@gmail.com>
2021-10-29 01:12:21 -0300

    Changes external link behavior

--- src/components/gui/baseButton/BaseButton.tsx
+++ src/components/gui/baseButton/BaseButton.tsx
@@ -7,16 +7,9 @@ import './ButtonIcon.css';
 export default function BaseButton(props: { text?: string, href: string, className?: string, svgIconPath?: string }): React.ReactElement {
     const { text, href, className, svgIconPath } = props;
     const toAppendClass = className ? ' ' + className : '';
-    const externalLink = href.startsWith('http');
-    const toLink = externalLink ? undefined : href;
-    const externalRedirect = !externalLink ? undefined : () => {
-        window.open(
-            href,
-            '_blank' // <- This is what makes it open in a new window.
-        );
-    };
+    const externalLink = href.startsWith('http') ? '_blank' : undefined ;
 
-    return <Link className={'button' + toAppendClass} to={{ pathname: toLink }} onClick={externalRedirect}>
+    return <Link className={'button' + toAppendClass} to={{ pathname: href }} target={externalLink}>
         {text && <div className='buttonText'>
             {text}
         </div>}
